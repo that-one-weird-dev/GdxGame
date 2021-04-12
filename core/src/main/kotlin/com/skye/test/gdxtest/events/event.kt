cf5eb6ef7e9d9b37b45acc43ae1f0a327297c570
N3rvy <manuelmanuel2003@gmail.com>
2021-04-12 23:33:20 +0200

    Added an event system

---
+++ core/src/main/kotlin/com/skye/test/gdxtest/events/event.kt
@@ -0,0 +1,39 @@
+package com.skye.test.gdxtest.events
+
+import com.badlogic.gdx.utils.ObjectMap
+import ktx.collections.GdxSet
+import ktx.collections.set
+import kotlin.reflect.KClass
+
+
+sealed class GameEvent {
+    object PlayerMove : GameEvent() {
+        val posX: Int = 0
+        val posY: Int = 0
+    }
+}
+
+interface GameEventListener {
+    fun onEvent(event: GameEvent? = null)
+}
+
+
+object GameEventManager {
+    private val listeners = ObjectMap<KClass<out GameEvent>, GdxSet<GameEventListener>>()
+
+    fun registerListener(type: KClass<out GameEvent>, listener: GameEventListener) {
+        var set = listeners[type]
+        if (set == null) {
+            set = GdxSet()
+            listeners[type] = set
+        }
+
+        set.add(listener)
+    }
+
+    fun dispatchEvent(type: KClass<out GameEvent>) {
+        listeners[type]?.forEach {
+            it.onEvent()
+        }
+    }
+}

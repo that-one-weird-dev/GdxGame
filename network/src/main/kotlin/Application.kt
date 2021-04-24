0db6cf1a188dbf998328845d4547f1e592247260
N3rvy <manuelmanuel2003@gmail.com>
2021-04-24 19:49:29 +0200

    Made the server a libgdx-headless module

---
+++ network/src/main/kotlin/Application.kt
@@ -0,0 +1,62 @@
+import com.badlogic.gdx.ApplicationListener
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.serialization.ClassResolvers
+import io.netty.handler.codec.serialization.ObjectDecoder
+import io.netty.handler.codec.serialization.ObjectEncoder
+
+
+private const val PORT = 8000
+
+class Application : ApplicationListener {
+
+    override fun create() {
+//        initializeServer()
+    }
+
+    override fun dispose() {}
+
+
+    private fun initializeServer() {
+        val bossGroup = NioEventLoopGroup()
+        val workerGroup = NioEventLoopGroup()
+
+        try {
+            val b = ServerBootstrap()
+            b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel::class.java)
+                .childHandler(object : ChannelInitializer<SocketChannel>() {
+                    override fun initChannel(ch: SocketChannel) {
+                        ch.pipeline().addLast(
+                            ObjectDecoder(ClassResolvers.softCachingResolver(ClassLoader.getSystemClassLoader())),
+                            ObjectEncoder(),
+                            EchoServerHandler(),
+                        )
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+
+            // Bind and start to accept incoming connections.
+            val f = b.bind(PORT).sync()
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync()
+        } finally {
+            workerGroup.shutdownGracefully()
+            bossGroup.shutdownGracefully()
+        }
+    }
+
+    // Useless methods (because this is headless)
+    override fun resize(width: Int, height: Int) {}
+    override fun render() {}
+    override fun pause() {}
+    override fun resume() {}
+}

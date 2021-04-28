d96a10f6948571747c90f9fde9391a463896e4c2
N3rvy <manuelmanuel2003@gmail.com>
2021-04-28 11:11:50 +0200

    Updated the main to use packet

--- core/src/main/kotlin/screen/MainScreen.kt
+++ core/src/main/kotlin/screen/MainScreen.kt
@@ -3,14 +3,18 @@ package screen
 import AnimationProvider
 import GameAnimation
 import Game
+import com.badlogic.gdx.math.Vector3
 import ecs.components.*
-import ecs.components.synchronization.ComponentPacker
 import ecs.components.synchronization.callMethod
 import ecs.components.synchronization.pack
+import ecs.components.synchronization.setField
 import ecs.createEntityWithId
+import ecs.systems.SmoothMoveSystem
 import ktx.ashley.with
 import ktx.log.debug
 import ktx.log.logger
+import packets.PacketCreateEntity
+import packets.with
 import kotlin.math.min
 
 private val LOG = logger<MainScreen>()
@@ -22,24 +26,23 @@ class MainScreen(game: Game) : AbstractScreen(game) {
     override fun show() {
         LOG.debug { "Main screen shown" }
 
-        val comp = FlipComponent()
-        println(comp.pack())
-        comp.callMethod("testMethod", "testParam")
-
-        engine.createEntityWithId {
-            with<TransformComponent> {
+        val packet = PacketCreateEntity.create {
+            with<IDComponent> {
+                id = "down"
+            }
+            with<TransformComponent>() {
                 setInitialPosition(8f, 4.5f, 0f)
                 size.set(2.5f, 2.5f)
             }
+            with<AnimationComponent> {
+                currentAnimation = GameAnimation.PLAYER_IDLE
+            }
             with<GraphicComponent>()
             with<PlayerComponent>()
-            with<AnimationComponent> {
-                animation = AnimationProvider.getAnimation(GameAnimation.PLAYER_IDLE)
-            }
-            with<FlipComponent>()
             with<MoveComponent>()
+            with<FlipComponent>()
         }
-    }
+        packet.execute(game)
 
     override fun render(delta: Float) {
         engine.update(min(MAX_DELTA_TIME, delta))

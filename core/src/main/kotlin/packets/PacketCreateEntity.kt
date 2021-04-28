f19cd79dcf18046cfbd520f976beb3a97ff74e29
N3rvy <manuelmanuel2003@gmail.com>
2021-04-28 11:00:17 +0200

    Created a configurator for the PacketCreateEntity

--- core/src/main/kotlin/packets/PacketCreateEntity.kt
+++ core/src/main/kotlin/packets/PacketCreateEntity.kt
@@ -1,17 +1,64 @@
 package packets
 
 import Game
-import ecs.createEntityWithId
+import com.badlogic.gdx.utils.reflect.ClassReflection
+import ecs.EntityComponent
+import ecs.components.synchronization.pack
+import ecs.components.synchronization.setField
+import ktx.ashley.entity
 
-//class PacketCreateEntity(
-//    val components: Array<>
-//) : Packet {
-//    override fun execute(game: Game) {
-//        val s: Synchronizable = components[0]
-//        game.engine.createEntityWithId {
-//            components.forEach {
-//                it.initialize(this)
-//            }
-//        }
-//    }
-//}
+class PacketCreateEntity(
+    val components: Array<Component>,
+) : Packet {
+    override fun execute(game: Game) {
+        val engine = game.engine
+
+        engine.entity {
+            // Code from ktx.ashley.engines (using this because i can not use generics)
+            components.forEach { comp ->
+                val component = try {
+                    engine.createComponent(comp.type)
+                        ?: throw NullPointerException("The component of ${comp.type} type is null.")
+                } catch (exception: Throwable) {
+                    exception.printStackTrace()
+                    return
+                }.apply {
+                    comp.fields.forEach { field ->
+                        this.setField(field.key, field.value)
+                    }
+                }
+                entity.add(component)
+            }
+        }
+    }
+
+    data class Component(val type: Class<out EntityComponent>, val fields: Map<String, Any?>)
+
+    data class Configurator(
+        val components: MutableList<Component> = mutableListOf()
+    ) {
+        inline fun <reified T : EntityComponent> with(configure: T.() -> Unit = {}) {
+            val comp = ClassReflection.newInstance(T::class.java)
+            comp.configure()
+
+            components.add(
+                Component(
+                    comp::class.java,
+                    comp.pack(),
+                )
+            )
+        }
+
+        fun toArray(): Array<Component> {
+            return components.toTypedArray()
+        }
+    }
+
+    companion object {
+        fun create(configure: Configurator.() -> Unit): PacketCreateEntity {
+            val conf = Configurator()
+            conf.configure()
+            return PacketCreateEntity(conf.toArray())
+        }
+    }
+}

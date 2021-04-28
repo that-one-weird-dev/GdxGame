650c28cb1edebbeb07f8f5d61b63459e57a4f0c5
N3rvy <manuelmanuel2003@gmail.com>
2021-04-28 12:28:56 +0200

    Reimplemented PacketUpdateComponent

--- core/src/main/kotlin/packets/PacketUpdateComponent.kt
+++ core/src/main/kotlin/packets/PacketUpdateComponent.kt
@@ -1,17 +1,20 @@
 package packets
 
 import Game
+import ecs.components.synchronization.getComponentMapper
+import ecs.components.synchronization.setField
 import ktx.ashley.get
 
-//data class PacketUpdateComponent(
-//    val entityId: String,
-//    val component: Synchronizable,
-//) : Packet {
-//    override fun execute(game: Game) {
-//        val entity = Game.entities[entityId]
-//        val comp = entity?.get(component.getMapper()) ?: return
-//
-//        if (comp is SynchronizedComponent)
-//            comp.synchronize(component)
-//    }
-//}
+data class PacketUpdateComponent(
+    val entityId: String,
+    val component: ComponentData,
+) : Packet {
+    override fun execute(game: Game) {
+        val entity = Game.entities[entityId]
+        val comp = entity?.get(component.type.getComponentMapper()) ?: return
+
+        component.fields.forEach {
+            comp.setField(it.key, it.value)
+        }
+    }
+}

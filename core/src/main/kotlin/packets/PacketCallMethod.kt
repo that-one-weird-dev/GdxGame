fd486f3b6e7689342f8ce0435eb7bc57942eb387
N3rvy <manuelmanuel2003@gmail.com>
2021-04-28 12:56:26 +0200

    Added packet for Calling a method

---
+++ core/src/main/kotlin/packets/PacketCallMethod.kt
@@ -0,0 +1,21 @@
+package packets
+
+import Game
+import com.badlogic.ashley.core.Component
+import ecs.components.synchronization.callMethod
+import ecs.components.synchronization.getComponentMapper
+import ktx.ashley.get
+
+class PacketCallMethod(
+    private val entityId: String,
+    private val componentType: Class<out Component>,
+    private val methodName: String,
+    private val args: Array<Any?> = arrayOf(),
+) : Packet {
+    override fun execute(game: Game) {
+        val entity = Game.entities[entityId]
+        val comp = entity?.get(componentType.getComponentMapper()) ?: return
+
+        comp.callMethod(methodName, *args)
+    }
+}

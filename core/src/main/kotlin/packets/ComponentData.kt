5b1121fbf5f7b5f0ade37ce766848a2876cff924
N3rvy <manuelmanuel2003@gmail.com>
2021-04-28 14:02:58 +0200

    Made ComponentData serializable

--- core/src/main/kotlin/packets/ComponentData.kt
+++ core/src/main/kotlin/packets/ComponentData.kt
@@ -3,8 +3,9 @@ package packets
 import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.utils.reflect.ClassReflection
 import ecs.components.synchronization.pack
+import java.io.Serializable
 
-data class ComponentData(val type: Class<out Component>, val fields: Map<String, Any?>) {
+data class ComponentData(val type: Class<out Component>, val fields: Map<String, Any?>) : Serializable {
     companion object {
         inline fun <reified T: Component> create(configure: T.() -> Unit = {}): ComponentData {
             val comp = ClassReflection.newInstance(T::class.java)
